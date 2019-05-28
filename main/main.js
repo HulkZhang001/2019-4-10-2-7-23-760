1e5b3c3efcc275aae6e31ff3712c52a29d898816
HulkZhang001 <530670864@qq.com>
2019-05-28 14:06:25 +0800

    made change05281406

--- main/main.js
+++ main/main.js
@@ -1,4 +1,15 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+	    var distance = inputs["distance"];
+		var parkTime = inputs["parkTime"];
+		var pay = 6;
+		pay += 0.25 * parkTime;
+
+		if(distance >= 2) {
+			pay += 0.8 * (distance - 2);
+			if(distance >= 8) {
+				pay += 0.8 * 0.5 * (distance - 8);
+			}
+		}
+
+		return Math.round(pay);
 };
